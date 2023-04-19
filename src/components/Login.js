0a840e010c2eef94b824afe911bbf25e63224b12
Lance Amolo <punchcode@Punchcodes-MacBook-Pro.local>
2023-04-18 18:15:38 -0700

    HTML and CSS done

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
+import logo from '../images/todo-list-logo.png';
+import '../styling/Login.css';
 
-export function Login(){
+
+export const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("The email is " + email);
+    }
+
     return (
-        <div>
-        Login Page
+        <div className='main-content'>
+            <div id='login'>
+                <img src={logo} id='logo' style={{ width: 300 }}/>
+                <form onSubmit={handleSubmit}>
+                    <input value={email} onChange={(e) => setEmail(e.target.value)} type='email' placeholder='Email' className='login-input' id='login-email'/><br/>
+                    <input value={password} onChange={(e) => setPassword(e.target.value)} type='password' placeholder='Password' className='login-input' id='login-password' /><br/>
+                    <button className='login-button' type='submit'>Submit</button>
+                </form>
+            <button className='login-button'>Register</button>
+            </div>
         </div>
     )
 }
