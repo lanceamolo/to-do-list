0dd66f3f5a3e993aef14384b6c2b2d0a1e8ae805
Kenjul <kenjul.biz@gmail.com>
2023-04-19 23:33:44 -0700

    routes set up

--- src/App.js
+++ src/App.js
@@ -1,29 +1,17 @@
 import React from "react";
-import { Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Login } from "./components/Login.jsx";
 import { Home } from "./components/Home.jsx";
-import { TaskDetails } from "./components/TaskDetails.jsx";
-import { Create } from "./components/Create.jsx";
 import "./App.css";
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <Login />
-        </Route>
-        <Route exact path="/home">
-          <Home />
-        </Route>
-        <Route exact path="/create">
-          <Create />
-        </Route>
-        <Route path="/task/:taskId">
-          <TaskDetails />
-        </Route>
-      </Switch>
-    </Router>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path='/' element={<Login />}/>
+        <Route path='/home' element={<Home/>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
