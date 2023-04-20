482c819b8a336352a6d514dcb4507892cc83a383
Kenjul <kenjul.biz@gmail.com>
2023-04-19 23:04:05 -0700

    home v1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import React from 'react';
-import { Login } from './components/Login.jsx';
-import './App.css';
+import React from "react";
+import { Router, Switch, Route } from "react-router-dom";
+import { Login } from "./components/Login.jsx";
+import { Home } from "./components/Home.jsx";
+import { TaskDetails } from "./components/TaskDetails.jsx";
+import { Create } from "./components/Create.jsx";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <Login />
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Login />
+        </Route>
+        <Route exact path="/home">
+          <Home />
+        </Route>
+        <Route exact path="/create">
+          <Create />
+        </Route>
+        <Route path="/task/:taskId">
+          <TaskDetails />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
