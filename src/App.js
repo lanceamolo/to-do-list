0ab626b9ac27777faf02075a2f2d122cb2ce0553
Kenjul <kenjul.biz@gmail.com>
2023-04-23 11:29:52 -0700

    need to fix TaskDetails and resolve app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Login } from "./components/Login.jsx";
 import { Home } from "./components/Home.jsx";
@@ -7,13 +7,41 @@ import { TaskDetails } from "./components/TaskDetails.jsx";
 import "./App.css";
 
 function App() {
+  const [task, setTask] = useState(null);
+
+  const handleEdit = (editedTask) => {
+    // code to update the task in the list or database
+    console.log("edited task:", editedTask);
+  };
+
+  const handleDelete = (taskId) => {
+    // code to delete the task from the list or database
+    console.log("deleted task id:", taskId);
+  };
+
+  const handleGoBack = () => {
+    // code to navigate back to the previous page
+    console.log("go back");
+  };
+
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route exact path='/' element={<Login />} />
-        <Route path='/home' element={<Home />} />
-        <Route path='/create' element={<Create />} />
-        <Route path='/task/:taskId' element={<TaskDetails /> } />
+        <Route exact path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/create" element={<Create />} />
+        <Route
+          path="/task/:taskId"
+          element={
+            <TaskDetails
+              task={task}
+              onEdit={handleEdit}
+              onDelete={handleDelete}
+              onGoBack={handleGoBack}
+            />
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
--- src/components/TaskDetails.jsx
+++ src/components/TaskDetails.jsx
@@ -1,11 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 
+export const TaskDetails = ({ task, onEdit, onDelete, onGoBack }) => {
+  const [editedTaskName, setEditedTaskName] = useState(task.name);
+  const [editedTaskDescription, setEditedTaskDescription] = useState(
+    task.description
+  );
+  const [editedTaskTime, setEditedTaskTime] = useState(task.time);
 
-export const TaskDetails = () => {
-    return (
-        <div>
-            <button>Delete</button>
-            <button>Edit</button>
-        </div>
-    );
-} 
+  const handleEdit = (event) => {
+    event.preventDefault();
+
+    const editedTask = {
+      id: task.id,
+      name: editedTaskName,
+      description: editedTaskDescription,
+      time: editedTaskTime,
+    };
+    onEdit(editedTask);
+  };
+
+  const handleDelete = (event) => {
+    event.preventDefault();
+    onDelete(task.id);
+  };
+
+  return (
+    <div>
+      <h1>Task Details</h1>
+
+      <form onSubmit={handleEdit}>
+        <label>
+          Task:
+          <input
+            type="text"
+            value={editedTaskName}
+            onChange={(event) => setEditedTaskDescription(event.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          Description:
+          <input
+            type="textarea"
+            value={editedTaskDescription}
+            onChange={(event) => setEditedTaskDescription(event.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          Time:
+          <input
+            type="time"
+            value={editedTaskTime}
+            onChange={(event) => setEditedTaskTime(event.target.value)}
+          />
+        </label>
+        <br />
+        <button type="submit">Save</button>
+        <button onClick={handleDelete}>Delete</button>
+        <button onClick={onGoBack}>Go Back</button>
+      </form>
+    </div>
+  );
+};
